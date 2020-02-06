f933d7b4f35b9925079cbebc94f654afb915a247
BlackMATov <blackmat@mail.ru>
2020-02-06 17:24:11 +0700

    update CI scripts

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 BUILD_DIR=`dirname "$BASH_SOURCE"`/../build
-mkdir -p $BUILD_DIR/debug
-cd $BUILD_DIR/debug
+mkdir -p $BUILD_DIR/Debug
+pushd $BUILD_DIR/Debug
 cmake -DCMAKE_BUILD_TYPE=Debug ../..
-cmake --build . -- -j8
-cd ../..
+cmake --build .
+popd
---
+++ scripts/build_debug_x86.bat
@@ -0,0 +1,13 @@
+@echo off
+set BUILD_DIR=%~dp0%\..\build
+mkdir %BUILD_DIR%\Debug\x86 || goto :error
+pushd %BUILD_DIR%\Debug\x86 || goto :error
+cmake ..\..\.. -A Win32 || goto :error
+cmake --build . --config Debug || goto :error
+popd || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
